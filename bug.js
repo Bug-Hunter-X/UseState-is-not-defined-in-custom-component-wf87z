ecbb864782e18fa1feb855131336b0cc2f5b868a
BugHunter-X <askzenai@gmail.com>
2025-02-02 08:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,23 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a paragraph of text</p>
+      <MyComponent/>
+    </div>
+  );
+}
+
+// components/MyComponent.js
+export default function MyComponent(){
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
